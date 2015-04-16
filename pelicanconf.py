442ea535b86581f08ff9049075fa0a564e7b0ddc
Mr. Byte <bytemr@gmail.com>
2015-04-15 20:36:34 -0600

    Initial site content.

---
+++ pelicanconf.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Joshua R. Rodgers'
+SITENAME = 'The Enginerd'
+SITEURL = ''
+
+SOURCE_URL = 'http://github.com/mr-byte/mr-byte.github.io'
+
+PATH = 'Programming'
+
+TIMEZONE = 'America/Denver'
+
+DEFAULT_LANG = 'en'
+
+FEED_DOMAIN = SITEURL
+FEED_ALL_ATOM = 'atom.xml'
+CATEGORY_FEED_ATOM = 'categories/%s/atom.xml'
+
+ARTICLE_URL = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+
+ARCHIVES_SAVE_AS = 'blog/archives/index.html'
+
+CATEGORY_URL = 'categories/{slug}/'
+CATEGORY_SAVE_AS = 'categories/{slug}/index.html'
+
+DEFAULT_PAGINATION = 10
